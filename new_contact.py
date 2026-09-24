ca05b841a6d8f6888abaca80f23ac35c9471668c
Roman <rgbalga@gmail.com>
2026-09-24 16:00:24 +0200

    все обновить

--- new_contact.py
+++ new_contact.py
@@ -1,12 +1,27 @@
-print("new contact text")
-import random
+from pathlib import Path
+contacts_path = Path("contacts.json")
+import json
+
+if not contacts_path.exists():
+    with open("contacts.json", "w", encoding="UTF-8") as contacts_file:
+        data_cont = contacts_file.read()
+
+else:
+    name = input("напишите имя ")
+    while True:
+        try:
+            phone = int(input("Введите номер телефона: "))
+            break
+        except ValueError:
+            print("Ошибка! Введите только цифры.")
+    comment = input("Напишите комментарий ")
 
 
-import time
-def count():
-    for i in range(20):
-        yield(i)
-        time.sleep(1)
+    with open("contacts.json", "a", encoding="UTF-8") as contacts_file:
+        phone_list = {"name": name, "phone": phone, "comment": comment}
+        json.dump(phone_list, contacts_file, ensure_ascii=False)
+        contacts_file.write("\n")
 
-for number in count():
-    print(number)
+with open("contacts.json", "r", encoding="UTF-8") as contacts_file:
+    data = contacts_file.read()
+print(data)
